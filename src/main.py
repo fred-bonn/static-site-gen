d4c669f0e78bb6011879e97f973646fb11694f5e
fred-bonn <sadpantz@gmail.com>
2026-03-15 21:21:53 +0100

    fixed bug where images and css was copied correctly to docs

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from static_copy import static_copy
 from page_generation import generate_pages_recursive
 
 static_path = "./static"
-public_path = "./public"
+docs_path = "./docs"
 
 def main():
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
     else:
         base_path = "/"
-    static_copy(static_path, public_path)
+    static_copy(static_path, docs_path)
     generate_pages_recursive("./content", "./template.html", "./docs", base_path)
 
 main()
